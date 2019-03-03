e069698455304310cc42cfd144ce590588182d87
Dmitry Nesterenko <nesterenko0602@gmail.com>
2019-03-04 00:58:33 +0200

    Checkbox component

--- src/components/ui/Checkbox.vue
+++ src/components/ui/Checkbox.vue
@@ -1,16 +1,40 @@
 <template>
-  <div class="wrapper">
-    Checkbox
-  </div>
+<div class="checkbox">
+  <input
+    type="checkbox"
+    v-bind:id="id"
+    v-bind:checked="checked"
+    v-on:change="$emit('change', { checked: $event.target.checked, id })"
+  >
+</div>
 </template>
 
 <script>
 export default {
-  name: 'Checkbox',
+  component: 'Checkbox',
+  model: {
+    prop: 'checked',
+    event: 'change',
+  },
+  props: {
+    checked: Boolean,
+    id: Number,
+  },
 };
 </script>
 
-<style scoped>
-  .wrapper {
+<style lang="scss" scoped>
+  .checkbox {
+    align-items: center;
+    display: flex;
+    flex: 0 0 auto;
+    height: 18px;
+    width: 18px;
+
+    input[type="checkbox"] {
+      cursor: pointer;
+      margin: 0;
+      transform: translate(25%, 0) scale(1.5);
+    }
   }
 </style>
